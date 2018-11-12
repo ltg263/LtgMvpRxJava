23c508c921c069ddb39d13e6e8792aa7a956de8c
litongge <ltg19870609>
2018-11-12 11:42:41 +0800

    完成网络请求操作

--- app/src/main/java/com/ltg/ltgmvprxjava/MainActivity.java
+++ app/src/main/java/com/ltg/ltgmvprxjava/MainActivity.java
@@ -2,6 +2,11 @@ package com.ltg.ltgmvprxjava;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.ltg.ltgmvprxjava.apiService.RetrofitUtil;
+import com.ltg.ltgmvprxjava.apiService.RxHttpParams;
+import com.ltg.ltgmvprxjava.baseManager.Constants;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +16,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    public void onClick(View view) {
+        RxHttpParams params = new RxHttpParams.Build()
+                .url(Constants.URL)
+//                .addQuery("key", key)
+                .build();
+        RetrofitUtil.request(params, String.class, new RetrofitUtil.HttpCallBackImpl<String>() {
+            @Override
+            public void onCompleted(String contactsBean) {
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/ltg/ltgmvprxjava/baseManager/BaseApp.java
@@ -0,0 +1,59 @@
+package com.ltg.ltgmvprxjava.baseManager;
+
+import android.app.Activity;
+import android.app.Application;
+
+import java.util.Stack;
+
+/**
+ * 描    述：Application
+ * 作    者：litongge
+ * 时    间：2016/7/28 17:19
+ */
+public class BaseApp extends Application {
+    private static Stack<Activity> activityStack;
+    private static BaseApp singleton;
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        AppConfig.initAppConfig(this);
+        singleton=this;
+
+    }
+    // Returns the application instance
+    public static BaseApp getInstance() {
+        return singleton;
+    }
+
+    /**
+     * add Activity 添加Activity到栈
+     */
+    public void addActivity(Activity activity){
+        if(activityStack ==null){
+            activityStack =new Stack<>();
+        }
+        activityStack.add(activity);
+    }
+
+    /**
+     * 退出应用程序
+     */
+    public void AppExit() {
+        try {
+            finishAllActivity();
+        } catch (Exception e) {
+        }
+    }
+    /**
+     * 结束所有Activity
+     */
+    public void finishAllActivity() {
+        for (int i = 0, size = activityStack.size(); i < size; i++) {
+            if (null != activityStack.get(i)) {
+                activityStack.get(i).finish();
+            }
+        }
+        activityStack.clear();
+    }
+
+}
