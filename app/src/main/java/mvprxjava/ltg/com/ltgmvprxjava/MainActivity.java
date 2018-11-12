331340ff8bd1aaf3e179c717b75a3acc79a619f6
litongge <ltg19870609>
2018-11-12 10:42:32 +0800

    新建空白项目

---
+++ app/src/main/java/mvprxjava/ltg/com/ltgmvprxjava/MainActivity.java
@@ -0,0 +1,14 @@
+package mvprxjava.ltg.com.ltgmvprxjava;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        撒旦发射点
+    }
+}
