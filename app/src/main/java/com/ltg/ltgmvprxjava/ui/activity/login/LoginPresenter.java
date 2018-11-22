545fc879d44e5b6374ef05702ce072c541bcee24
litongge <ltg19870609>
2018-11-22 10:02:00 +0800

    mvp

--- app/src/main/java/com/ltg/ltgmvprxjava/ui/activity/login/LoginPresenter.java
+++ app/src/main/java/com/ltg/ltgmvprxjava/ui/activity/login/LoginPresenter.java
@@ -1,6 +1,9 @@
 package com.ltg.ltgmvprxjava.ui.activity.login;
 
 
+import com.ltg.ltgmvprxjava.apiService.RetrofitUtil;
+import com.ltg.ltgmvprxjava.apiService.RxHttpParams;
+import com.ltg.ltgmvprxjava.baseManager.Constants;
 import com.ltg.ltgmvprxjava.mvp.BasePresenterImpl;
 
 /**
@@ -15,23 +18,21 @@ public class LoginPresenter extends BasePresenterImpl<LoginContract.View> implem
         if(name.equals("123") && password.equals("abc")){
             mView.loginSucceed("");
         }else{
-            mView.loginFailed("登陆失败哦");
+            RxHttpParams params = new RxHttpParams.Build()
+                    .url(Constants.URL)
+                    .build();
+            RetrofitUtil.request(params, String.class, new RetrofitUtil.HttpCallBackImpl<String>() {
+                @Override
+                public void onCompleted(String contactsBean) {
+                    mView.loginFailed(contactsBean);
+                }
+
+                @Override
+                public void onError(String message) {
+                    super.onError(message);
+                    mView.loginFailed(message);
+                }
+            });
         }
     }
-//        RxHttpParams params = new RxHttpParams.Build()
-//                .url(Constants.URL)
-//                .addQuery("name", name)
-//                .addQuery("password", password)
-//                .build();
-//        RetrofitUtil.request(params, String.class, new RetrofitUtil.HttpCallBackImpl<String>() {
-//            @Override
-//            public void onCompleted(String contactsBean) {
-//                if(contactsBean.equals("123") && contactsBean.equals("aaa")){
-//                    mView.loginSucceed(contactsBean);
-//                }else{
-//                    mView.loginFailed("登陆失败哦");
-//                }
-//            }
-//        });
-//    }
 }
--- app/src/main/java/com/ltg/ltgmvprxjava/ui/fragment/home/HomePresenter.java
+++ app/src/main/java/com/ltg/ltgmvprxjava/ui/fragment/home/HomePresenter.java
@@ -1,6 +1,9 @@
 package com.ltg.ltgmvprxjava.ui.fragment.home;
 
 
+import com.ltg.ltgmvprxjava.apiService.RetrofitUtil;
+import com.ltg.ltgmvprxjava.apiService.RxHttpParams;
+import com.ltg.ltgmvprxjava.baseManager.Constants;
 import com.ltg.ltgmvprxjava.mvp.BasePresenterImpl;
 
 /**
@@ -15,7 +18,21 @@ public class HomePresenter extends BasePresenterImpl<HomeContract.View> implemen
         if(sta == true){
             mView.startLoginC("成功2222");
         }else{
-            mView.startLoginS("失败2222");
+            RxHttpParams params = new RxHttpParams.Build()
+                    .url(Constants.URL)
+                    .build();
+            RetrofitUtil.request(params, String.class, new RetrofitUtil.HttpCallBackImpl<String>() {
+                @Override
+                public void onCompleted(String contactsBean) {
+                    mView.startLoginC(contactsBean);
+                }
+
+                @Override
+                public void onError(String message) {
+                    super.onError(message);
+                    mView.startLoginC(message);
+                }
+            });
         }
     }
 }
